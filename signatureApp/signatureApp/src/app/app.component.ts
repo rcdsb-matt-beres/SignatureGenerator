5aa1dbe1b4874a9fabfb5d1102ae2af7897f130d
rcdsb-matt-beres <beresm@rcdsb.on.ca>
2025-03-18 15:22:41 -0400

    add instructions

--- signatureApp/signatureApp/src/app/app.component.ts
+++ signatureApp/signatureApp/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
